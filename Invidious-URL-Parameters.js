ebc6071ab3f6f7b4d0da3bf58c0a41e4b8c81c7e
MintMain21 <91362684+MintMain21@users.noreply.github.com>
2023-03-05 17:36:43 -0800

    Minor Typo Editing in the description.
    Thanks to Gitoffthelawn for submitting a pull request to edit the Readme, which is near identical to the script's description in the script itself.

--- Invidious-URL-Parameters.js
+++ Invidious-URL-Parameters.js
@@ -13,18 +13,18 @@
 // @license      MIT
 // @homepage     https://github.com/MintMain21/Invidious-URL-Parameters-Userscript
 // ==/UserScript==
-/** This Userscript is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout but rewritten to instead
-redirect Invidious Video URLs with URL parameters to enforce preferences like video quality, visibility of comments, etc, without using browser cookies.
+/** This userscript is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout but rewritten to instead
+redirect Invidious video URLs with URL parameters to enforce preferences like video quality, visibility of comments, etc, without using browser cookies.
 
 To edit the enforced URL parameters, change both (!url.includes("[URL Parameters Here]") and (window.location.replace(url + "[URL Parameters Here]").
 
-To edit the invidious instances this script applies to, edit the above domains.
+To edit the Invidious instances this script applies to, edit the above domains.
 
 Anytime you load a video URL on your desired Invidious instance, this script will check the URL for the desired parameters, and apply them if not found.
 
-It is reccomended that you use this script in combination with https://github.com/dybdeskarphet/privacy-redirector and https://codeberg.org/mthsk/userscripts/src/branch/master/simple-sponsor-skipper
+It is recommended that you use this script in combination with https://github.com/dybdeskarphet/privacy-redirector and https://codeberg.org/mthsk/userscripts/src/branch/master/simple-sponsor-skipper
 
-For more information about Invidious URL Parameters and the various options, see https://docs.invidious.io/url-parameters/
+For more information about Invidious URL parameters and the various options, see https://docs.invidious.io/url-parameters/
  */
 
 function getCurrentURL () {
