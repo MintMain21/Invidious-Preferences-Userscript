0c8f72c0af0dabad05f69d4fe0e7ac02eae2cbab
MintMain21 <91362684+MintMain21@users.noreply.github.com>
2023-04-06 12:46:55 -0700

    Created single

--- Invidious-URL-Parameters.js
+++ Invidious-URL-Parameters.js
@@ -53,16 +53,16 @@
 // ==/UserScript==
 /** This userscript is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout but rewritten to instead
 redirect Invidious video URLs with URL parameters to enforce preferences like video quality, visibility of comments, etc, without using browser cookies.
-
-To edit the enforced URL parameters, change both (!url.includes("[URL Parameters Here]") and (window.location.replace(url + "[URL Parameters Here]").
-
-To edit the Invidious instances this script applies to, edit the above domains.
-
+For more information about Invidious URL parameters and the various options, see https://docs.invidious.io/url-parameters/
+To edit the enforced URL parameters, change videosettings below.
+*/
+let videosettings = "&quality=dash&quality_dash=480&related_videos=false&comments=false&player_style=youtube"
+/**
 Anytime you load a video URL on your desired Invidious instance, this script will check the URL for the desired parameters, and apply them if not found.
-
+To edit the Invidious instances this script applies to, edit the above domains.
 It is recommended that you use this script in combination with https://github.com/dybdeskarphet/privacy-redirector and https://codeberg.org/mthsk/userscripts/src/branch/master/simple-sponsor-skipper
 
-For more information about Invidious URL parameters and the various options, see https://docs.invidious.io/url-parameters/
+
  */
 
 function getCurrentURL () {
@@ -75,7 +75,7 @@ const url = getCurrentURL();
 (function() {
     'use strict';
 
-    if (url.includes("v=") && !url.includes("&quality=dash&quality_dash=480&related_videos=false&comments=false&player_style=youtube")) {
-        window.location.replace(url + "&quality=dash&quality_dash=480&related_videos=false&comments=false&player_style=youtube");
+    if (url.includes("v=") && !url.includes(videosettings)) {
+        window.location.replace(url + videosettings);
     }
 })();
