0218de7cc71a1747ff03b54196ab933d139b7a7f
MintMain21 <91362684+MintMain21@users.noreply.github.com>
2023-09-10 13:46:20 -0700

    Update Invidious-Preferences.js to Version 3
    Bug fixes, updated list of default instances (with defunct instances removed), and new option to autoplay playlist videos

--- Invidious-Preferences.js
+++ Invidious-Preferences.js
@@ -1,18 +1,15 @@
 // ==UserScript==
 // @name         Invidious Preferences
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      3
 // @description  Redirect Invidious Videos with URL Parameters
 // @author       MintMain21
-// @match        *://inv.odyssey346.dev/*
 // @match        *://invidious.baczek.me/*
 // @match        *://iteroni.com/*
 // @match        *://invidious.namazso.eu/*
 // @match        *://iv.ggtyler.dev/*
 // @match        *://yewtu.be/*
 // @match        *://vid.puffyan.us/*
-// @match        *://inv.riverside.rocks/*
-// @match        *://watch.thekitty.zone/*
 // @match        *://y.com.sb/*
 // @match        *://invidious.nerdvpn.de/*
 // @match        *://invidious.tiekoetter.com/*
@@ -20,25 +17,28 @@
 // @match        *://invidious.flokinet.to/*
 // @match        *://inv.bp.projectsegfau.lt/*
 // @match        *://inv.vern.cc/*
-// @match        *://invidious.sethforprivacy.com/*
 // @match        *://invidious.projectsegfau.lt/*
 // @match        *://yt.funami.tech/*
 // @match        *://invidious.lunar.icu/*
 // @match        *://invidious.privacydev.net/*
-// @match        *://invidious.slipfox.xyz/*
 // @match        *://invidious.esmailelbob.xyz/*
-// @match        *://iv.melmac.space/*
 // @match        *://invidious.snopyta.org/*
-// @match        *://invidious.weblibre.org/*
 // @match        *://invidious.kavin.rocks/*
 // @match        *://yt.oelrichsgarcia.de/*
 // @match        *://invidious.privacydev.net/*
-// @match        *://invidious.vpsburti.com/*
 // @match        *://inv.zzls.xyz/*
-// @match        *://invidious.0011.lt/*
-// @match        *://ytb.trom.tf/*
 // @match        *://vid.priv.au/*
 // @match        *://invidious.fdn.fr/*
+// @match        *://inv.tux.pizza/*
+// @match        *://invidious.nogafa.org/*
+// @match        *://inv.pistasjis.net/*
+// @match        *://invidious.protokolla.fi/*
+// @match        *://inv.in.projectsegfau.lt/*
+// @match        *://inv.citw.lgbt/*
+// @match        *://iv.nboeck.de/*
+// @match        *://invidious.private.coffee/*
+// @match        *://invidious.asir.dev/*
+// @match        *://invidious.io.lol/*
 // @match        *://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion/*
 // @match        *://w6ijuptxiku4xpnnaetxvnkc5vqcdu7mgns2u77qefoixi63vbvnpnqd.onion/*
 // @match        *://kbjggqkzv65ivcqj6bumvp337z6264huv5kpkwuv6gu5yjiskvan7fad.onion/*
@@ -57,7 +57,7 @@
 // @license      MIT
 // @homepage     https://github.com/MintMain21/Invidious-URL-Parameters-Userscript
 // ==/UserScript==
-/** This userscript is written to enforce invidious preferences such as theme, language, video quality, visibility of comments, etc, through URL parameters and without using browser cookies.
+/** This userscript is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout but rewritten to enforce video playback preferences as video quality, visibility of comments, etc, through URL parameters and without using browser cookies.
 For more information about Invidious URL parameters and the various options, see https://docs.invidious.io/url-parameters/
 To edit the enforced URL parameters, change the setting strings below. Keep options in their appropriate catagories (URL Parameters affecting video playback should only be in videosettings, etc). You can keep a setting option blank if you would rather not append any URL parameters.
 */
@@ -66,14 +66,21 @@ let searchsettings = "region=JP"
 let videosettings = "related_videos=false&comments=false&quality=hd720&player_style=invidious&extend_desc=true"
 let trendingsettings = "type=Music"
 /**
+Anytime you load a video URL on your desired Invidious instance, this script will check the URL for the desired parameters, and apply them if not found.
 To edit the Invidious instances this script applies to, edit the domains above.
 You may also edit the Homepage by changing the variable below (options include "search", "popular", and "trending"). */
 let homepage = "search"
+/** You may also set it so videos in a single playlist play automatically by setting the toggle below. */
+
+let PlaylistAutoplay = "true"
+
+
 /**
+
 It is recommended that you use this script in combination with https://github.com/dybdeskarphet/privacy-redirector and https://codeberg.org/mthsk/userscripts/src/branch/master/simple-sponsor-skipper
 
 
-This script can also redirect Invidious Feeds to their YouTube equivelants so that URLs from RSS Feeds can be easily redirected to the most convenient instance. To disable this, set rssRedirect below to "false"*/
+This script can also redirect Invidious Feeds to their YouTube equivelants so that URLs from RSS Feeds can be easily redirected to the most convenient instance. To disable this, set rssRedirect below to "false". */
 let rssRedirect = "true"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,7 +102,7 @@ const url = getCurrentURL();
 
             else if(!url.includes(appearencesettings)) {
                 window.location.replace(url + ("&" + appearencesettings));
-              
+
     }
 
     if (url.includes("q=") && !url.includes(searchsettings)) {
@@ -125,7 +132,11 @@ const url = getCurrentURL();
     if (url.includes("feed/trending") && !url.includes(trendingsettings)) {
         window.location.replace(url + ("?" + appearencesettings + "&" + trendingsettings));
     }
-    if (url.includes("v=") && !url.includes(videosettings)) {
+
+    if (url.includes("v=") && url.includes("&list=") && !url.includes(videosettings) && PlaylistAutoplay.includes("true")) {
+        window.location.replace(url + ("&" + appearencesettings + "&" + videosettings + "&autoplay=1"));
+    }
+    else if (url.includes("v=") && !url.includes(videosettings)) {
         window.location.replace(url + ("&" + appearencesettings + "&" + videosettings));
     }
 
@@ -133,7 +144,8 @@ const url = getCurrentURL();
 
 var InvidiousLinks = Array.from(document.links).filter(link => link.href.includes(window.location.hostname));
     for (var i = 0; i < InvidiousLinks.length; i++) {
-      
+
+
         if(!InvidiousLinks[i].href.includes("watch") && !InvidiousLinks[i].href.includes("preferences") && !InvidiousLinks[i].href.includes("channel") && !InvidiousLinks[i].href.includes("search") && !InvidiousLinks[i].href.includes("feed") && !InvidiousLinks[i].href.includes("playlist") && homepage.includes("search") && !InvidiousLinks[i].href.includes("embed")){
             InvidiousLinks[i].href = InvidiousLinks[i].href + ("search")
         }
@@ -152,11 +164,17 @@ var InvidiousLinks = Array.from(document.links).filter(link => link.href.include
         else if (!InvidiousLinks[i].href.includes("feed/channel") && !InvidiousLinks[i].href.includes("feed/playlist") && !InvidiousLinks[i].href.includes("v=") && !InvidiousLinks[i].href.includes("embed")){
             InvidiousLinks[i].href = InvidiousLinks[i].href + ("?" + appearencesettings)
         }
+
          if(InvidiousLinks[i].href.includes("v=") && InvidiousLinks[i].href.includes("&t=")){
             InvidiousLinks[i].href = InvidiousLinks[i].href.replace("t=", appearencesettings + "&" + videosettings + "&t=")
         }
 
-        else if (InvidiousLinks[i].href.includes("v=") && !InvidiousLinks[i].href.includes(videosettings)){
+          else if (InvidiousLinks[i].href.includes("v=") && InvidiousLinks[i].href.includes("&list=") && !InvidiousLinks[i].href.includes(videosettings) && !InvidiousLinks[i].href.includes("embed") && PlaylistAutoplay.includes("true")){
+                 InvidiousLinks[i].href = InvidiousLinks[i].href + ("&"+ appearencesettings + "&" + videosettings + "&autoplay=1")
+        }
+
+
+        else if (InvidiousLinks[i].href.includes("v=") && !InvidiousLinks[i].href.includes(videosettings) && !InvidiousLinks[i].href.includes("embed")){
                  InvidiousLinks[i].href = InvidiousLinks[i].href + ("&"+ appearencesettings + "&" + videosettings)
         }
                    if(InvidiousLinks[i].href.includes("feed/channel") && rssRedirect.includes("true")){
